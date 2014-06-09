6247afa089f363e40f3c8b3f5f94fd7ed7cddd15
Samuel Hinton <samuelreay@gmail.com>
2014-06-09 21:25:18 +1000

    First commit.

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var app = angular.module("phys2020", ["ui.bootstrap"]);
+
+function MainController($scope, $timeout) {
+    MathJax.Hub.Config({
+        extensions: ["tex2jax.js"],
+        jax: ["input/TeX", "output/HTML-CSS"],
+        tex2jax: {
+            inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+            displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+            processEscapes: true
+        },
+        "HTML-CSS": { availableFonts: ["TeX"] }
+    });
+    $scope.active = 'Week 2';
+    $scope.menu = [
+        {label: 'Week 1', partial: 'partials/week1.html'},
+        {label: 'Week 2', partial: 'partials/week2.html'},
+        {label: 'Week 3', partial: 'partials/week3.html'}
+    ];
+    $scope.getActive = function() {
+        for (var i = 0; i < $scope.menu.length; i++) {
+            if ($scope.menu[i].label == $scope.active) {
+                return $scope.menu[i].partial;
+            }
+        }
+        return "";
+    }
+    $scope.setActive = function(v) {
+        $scope.active = v;
+        $timeout(function() {
+            MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+        }, 500, false);
+    }
+
+    $timeout(function() {
+        MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+    }, 500, false);
+}
